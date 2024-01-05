8378289475b668fe86712a0c985eab3e7de5476c
Sushant Agawane <rkmail289@gmail.com>
2024-01-05 22:14:09 +0530

    Adds routes

---
+++ api/routes/shorten.go
@@ -0,0 +1,18 @@
+package routes
+
+import "time"
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+// * Rate Limit how many attempts left & RateLimitRest to reset the limit.
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
